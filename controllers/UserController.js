168261497c7ca77c20d409ff181c3dbfac247e10
Jvjuino92 <jvmmutiz@gmail.com>
2022-06-23 09:21:52 +0100

    Delete button working!

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -214,7 +214,7 @@ class UserController {
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>
         `;
 
@@ -228,8 +228,19 @@ class UserController {
 
     addEventsTr(tr) {
 
-        tr.querySelector(".btn-edit").addEventListener("click", e => {
+        tr.querySelector(".btn-delete").addEventListener("click", e => {
 
+            if (confirm("Do you really wish to delete?")) {
+
+                tr.remove();
+                this.updateCount();
+
+            }
+
+        });
+
+        tr.querySelector(".btn-edit").addEventListener("click", e => {
+        
             let json = JSON.parse(tr.dataset.user);
 
             this.formUpdateEl.dataset.trIndex = tr.sectionRowIndex;
